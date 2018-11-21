58b3ef3b58f4dc4333ce96ef79e7814f26b0793b
Lesley <lesleydreyer@hotmail.com>
2018-11-20 22:30:00 -0700

    added runserver and closeserver functions

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-module.exports = {
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3001'
-}
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3001';
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+    'mongodb://localhost/trail-time-db';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+    'mongodb://localhost/test-trail-time-db';
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
-const { CLIENT_ORIGIN } = require('./config');
+const { CLIENT_ORIGIN, PORT, DATABASE_URL, TEST_DATABASE_URL } = require('./config');
+//const PORT = process.env.PORT || 3000;
 
 app.use(
     cors({
@@ -9,14 +11,75 @@ app.use(
     })
 );
 
-
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.get('/api/*', (req, res) => {
-    res.json({ ok: true });
+    return res.json({ ok: true });
+});
+
+app.use('*', (req, res) => {
+    return res.status(404).json({
+        message: 'Not Found'
+    });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-module.exports = { app };
+
+let server;
+
+function runServer(testEnv) {
+    let mongoUrl;
+
+    if (testEnv) {
+        mongoUrl = TEST_DATABASE_URL;
+    } else {
+        mongoUrl = DATABASE_URL;
+    }
+    console.log(mongoUrl);
+    return new Promise((resolve, reject) => {
+        mongoose.connect(mongoUrl, {
+            useNewUrlParser: true
+        }, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app
+                .listen(PORT, () => {
+                    console.log(`Your app is listening on port ${PORT}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+//if (require.main === module) {
+//    runServer().catch(err => console.error(err));
+//}
+
+//module.exports = { app };
 //https://stormy-falls-76813.herokuapp.com/ is heroku
+
+module.exports = {
+    app,
+    runServer,
+    closeServer
+};
