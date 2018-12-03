185a8661858d95a28bf755e1b3d9adc5b0b6ea95
Lesley <lesleydreyer@hotmail.com>
2018-12-02 22:05:04 -0700

    worked on cloudinary

--- app/cloudinary/imageRouter.js
+++ app/cloudinary/imageRouter.js
@@ -1,47 +1,19 @@
-require('dotenv').config();
-const os = require('os');
 const express = require('express');
-const cors = require('cors');
 const cloudinary = require('cloudinary');
-const multer = require('multer');
-const upload = multer({ dest: os.tmpdir() });
-//const formData = require('express-form-data')
-
-//const { User } = require('../user/userModel');
-const imageRouter = express.Router();
-//const HTTP_STATUS_CODES = require('../httpStatusCodes');
-
-
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET
 })
-
-imageRouter.use(
-    cors({
-        origin: process.env.CLIENT_ORIGIN
-    })
-);
-
-imageRouter.use(upload.any());//or could do formData instead of multer imageRouter.use(formData.parse())
-imageRouter.use(express.json());
-
+const imageRouter = express.Router();
 imageRouter.get('/wake-up', (req, res) => res.send('👌'));
-
 imageRouter.post('/image-upload', (req, res) => {
     const values = Object.values(req.files);
     console.log('values', values)
     const promises = values.map(image =>
         cloudinary.uploader.upload(image.path));
-    console.log(image.path)
+    //console.log(image.path)
     Promise.all(promises).then(results => res.json(results));
 });
-
-//return User.findById(req.body.trail)
-//  .then(user => {
-//user
-//})
-
 
 module.exports = { imageRouter };
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 require('dotenv').config();
-const express = require('express');
+const os = require('os');
+
 const cloudinary = require('cloudinary');
-const formData = require('express-form-data');
-const app = express();
-const morgan = require('morgan');
 const mongoose = require('mongoose');
+
+const express = require('express');
+// Middleware
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
+const formData = require('express-form-data');
+const multer = require('multer');
+const upload = multer({ dest: os.tmpdir() });
+
 const { CLIENT_ORIGIN, PORT, DATABASE_URL, TEST_DATABASE_URL } = require('./config');
 const HTTP_CODES = require('./app/httpStatusCodes').HTTP_STATUS_CODES;
-console.log('DATABASE_URL', DATABASE_URL);
-mongoose.set('useCreateIndex', true);
-const bodyParser = require('body-parser');
+
 const { userRouter } = require('./app/user/index');
 const { trailRouter } = require('./app/trails/index');
 const { imageRouter } = require('./app/cloudinary/index');
 const { commentRouter } = require('./app/comments/index');
 const { authRouter, localStrategy, jwtStrategy } = require('./app/auth');
+
+console.log('DATABASE_URL', DATABASE_URL);
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+})
+mongoose.set('useCreateIndex', true);
+const app = express();
 
 let server;
 
@@ -24,10 +39,11 @@ passport.use(localStrategy); // Configure Passport to use our localStrategy when
 passport.use(jwtStrategy); // Configure Passport to use our jwtStrategy when receving JSON Web Tokens
 
 app.use(cors({ origin: CLIENT_ORIGIN }));
-app.use(express.json());
 app.use(morgan('common'));
-app.use(bodyParser.json());
-app.use(function (req, res, next) {
+app.use(express.json()); // You can also use: // app.use(bodyParser.json());
+app.use(upload.any()); // or could do formData instead of multer imageRouter.use(formData.parse())
+
+/*app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
@@ -35,12 +51,12 @@ app.use(function (req, res, next) {
         return res.send(204);
     }
     next();
-});
+});*/
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/trail', trailRouter);
-app.use('api/comments/:trailid', commentRouter);
-app.use('api/images/:trailid', imageRouter);
+app.use('/api/comments/:trailid', commentRouter);
+app.use('/api/images/:trailid', imageRouter);
 
 const jwtAuth = passport.authenticate('jwt', {
     session: false
